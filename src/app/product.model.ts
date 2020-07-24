3e22e158392693aac83bd076778f1bffe9334d05
oksanabansal <oksana.bansal@gmail.com>
2020-07-24 20:28:47 +0200

    working phase1

--- src/app/product.model.ts
+++ src/app/product.model.ts
@@ -1,12 +1,12 @@
-/**
- * Provides a `Product` object
- */
 export class Product {
+  public imageUrl: string;
+
   constructor(
     public sku: string,
     public name: string,
-    public imageUrl: string,
+    imageUrl: string,
     public department: string[],
     public price: number) {
+    this.imageUrl = imageUrl;
   }
 }
